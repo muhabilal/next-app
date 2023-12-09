c6b9ac5215c79a398e388918136f79817ca669cf
M Bilal <bilaldarmuhammad@gmail.com>
2023-12-09 14:19:20 +0500

    git init

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,17 @@
+"use client";
+import React from "react";
+
+const AddToCart = () => {
+  return (
+    <div>
+      <button
+        className="btn btn-primary"
+        onClick={() => console.log("Click me")}
+      >
+        Add too Cart
+      </button>
+    </div>
+  );
+};
+
+export default AddToCart;
